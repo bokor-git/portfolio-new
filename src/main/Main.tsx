94df7e53034c28c8123685181b22f0dc508bf519
bokor-git <bogdankorolbk@gmail.com>
2020-10-05 17:42:54 +0300

    about part beta done

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
     return <div className={style.main}>
         <div className={styleContainer.container}>
             <div className={style.text}>
-                <p className={style.top}>Hello, I'm <span className={style.highlight}>Bohdan Korol </span>. <br/>
+                <p className={style.top}>Hello, I'm <span className={style.highlight}>Bohdan Korol</span>. <br/>
                  I'm a front-end web developer.</p>
             </div>
             <div className={style.button}>View my work </div>
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -4,26 +4,110 @@ import styleContainer from "../common/styles/Container.module.css"
 import Skill from "./Skill/Skill";
 import {Line} from "../common/components/Line";
 
-const description = "Lorem ipsum dolor sit amet, consectetur adipisicing, sit amet, consectetur adipisicing, sit amet, consectetur adipisicing "
+
+const description = "Fast load times and lag free interaction, my highest priority."
 
 
 const Skills = () => {
     return <div className={style.skillsBlock}>
         <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-            <h2 className={style.title}>Skills</h2>
+            <h2 className={style.title}>ABOUT</h2>
             <Line/>
             <div className={style.skills}>
                 <Skill title="HTML" description={description}
                        icon={"https://pngimage.net/wp-content/uploads/2018/06/html-icon-png-6.png"}/>
                 <Skill title="CSS" description={description}
                        icon={"https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/9939568401548330925-512.png"}/>
-                <Skill title="JS" description={description} icon={"https://img.icons8.com/all/500/javascript.png"}/>
                 <Skill title="ReactJS" description={description}
                        icon={"https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/react-512.png"}/>
-                <Skill title="Redux" description={description} icon={"https://img.icons8.com/all/500/redux.png"}/>
                 <Skill title="NodeJS" description={description}
                        icon={"https://cdn.onlinewebfonts.com/svg/img_189697.png"}/>
             </div>
+            <div className={style.about}>
+                <div className={style.profile}>
+                    <img src="http://findmatthew.com/img/me.png" alt="profile photo" className={style.photo}/>
+                    <div className={style.title}>Who's this guy?</div>
+                    <div className={style.description}>
+                        <div>I'm a Front-End Developer from Ukraine <br/>
+                            I have serious passion for UI effects, animations and creating intuitive, dynamic user experiences.
+                        </div>
+                    </div>
+                </div>
+                <div className={style.info}>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>HTML</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>JavaScript</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+                    <div className={style.bar}>
+                        <span className={style.span}>90%</span>
+                        <div className={style.fill}>
+                            <div className={style.tag}>CSS</div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     </div>
 }
