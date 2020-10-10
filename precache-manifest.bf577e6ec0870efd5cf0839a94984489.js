afbc0153cf8f4c48108017949b9120c78a5d0779
bokor-git <bogdankorolbk@gmail.com>
2020-10-10 20:15:25 +0300

    Updates

---
+++ precache-manifest.bf577e6ec0870efd5cf0839a94984489.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3dfed114ed3e4a1408fac55704926f8",
+    "url": "/portfolio-new/index.html"
+  },
+  {
+    "revision": "e58abb193f7e32ced49b",
+    "url": "/portfolio-new/static/css/main.2d3f68bd.chunk.css"
+  },
+  {
+    "revision": "cdce7de86757524a57b4",
+    "url": "/portfolio-new/static/js/2.e7d1d98b.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/portfolio-new/static/js/2.e7d1d98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58abb193f7e32ced49b",
+    "url": "/portfolio-new/static/js/main.ad6b200c.chunk.js"
+  },
+  {
+    "revision": "e091338d7d49b6d250b8",
+    "url": "/portfolio-new/static/js/runtime-main.fff1fd40.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/portfolio-new/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "5f02ff6100dcd17dcd16a5b0a809be4a",
+    "url": "/portfolio-new/static/media/counter.5f02ff61.png"
+  },
+  {
+    "revision": "d78c4876cccac8ecb0342035a5bf9b54",
+    "url": "/portfolio-new/static/media/instagram.d78c4876.png"
+  },
+  {
+    "revision": "a31b9e8802ea9bc4f950d5a90105e2b6",
+    "url": "/portfolio-new/static/media/messenger.a31b9e88.png"
+  },
+  {
+    "revision": "6deab98c113244322d9cd00d119917ed",
+    "url": "/portfolio-new/static/media/social-network.6deab98c.png"
+  },
+  {
+    "revision": "2c3aa24e129d63fd52d01785dc152eb7",
+    "url": "/portfolio-new/static/media/telegram.2c3aa24e.png"
+  },
+  {
+    "revision": "650d1708b25c2082180a9514a35b8077",
+    "url": "/portfolio-new/static/media/todolist.650d1708.png"
+  },
+  {
+    "revision": "a4d3edecb1d39ae389b8173d7a98f6b0",
+    "url": "/portfolio-new/static/media/viber.a4d3edec.png"
+  }
+]);
