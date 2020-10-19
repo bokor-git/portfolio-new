49f1703c90a1fb5c861d6173217922010464ded6
bokor-git <bogdankorolbk@gmail.com>
2020-10-19 14:50:41 +0300

    fix errors in contact form

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -7,7 +7,7 @@ import {Formik} from "formik";
 import Fade from "react-reveal/Fade";
 // @ts-ignore
 import Zoom from 'react-reveal/Zoom';
-import contact from  "../common/icons/contact.svg"
+import contact from "../common/icons/contact.svg"
 import {contactFormAPI} from "../api/contacts-form";
 
 const Contacts = () => {
@@ -28,15 +28,16 @@ const Contacts = () => {
                     <Formik
                         initialValues={{name: '', email: '', text: ""}}
                         onSubmit={(values, {setSubmitting}) => {
-                            contactFormAPI.postInfo(values.name,values.email,values.text).then(res=>console.log(res))}}
+                            contactFormAPI.postInfo(values.name, values.email, values.text).then(res => console.log(res))
+                        }}
                         validate={values => {
                             let errors: { name?: string, email?: string, text?: string } = {};
                             if (!values.name) {
-                                errors.name = 'Name is Required';
+                                errors.name = 'Name is Required!';
                             } else if (!values.email) {
-                                errors.email = 'Invalid email address';
+                                errors.email = 'Invalid email address!';
                             } else if (!values.text) {
-                                errors.text = 'Text is Required';
+                                errors.text = 'Text is Required!';
                             }
                             return errors;
                         }}
@@ -58,7 +59,6 @@ const Contacts = () => {
                                        onChange={handleChange}
                                        onBlur={handleBlur}
                                        value={values.name}/>
-                                {errors.name && touched.name && errors.name}
                                 <input
                                     placeholder={"Email"}
                                     className={style.formInput}
@@ -68,13 +68,12 @@ const Contacts = () => {
                                     onBlur={handleBlur}
                                     value={values.email}
                                 />
-                                {errors.email && touched.email && errors.email}
                                 <textarea name="text"
                                           onChange={handleChange}
                                           onBlur={handleBlur}
                                           value={values.text} placeholder={"Enter your massage..."}
                                           className={style.formMessage}/>
-                                {errors.text && touched.text && errors.text}
+                                <div className={style.error}> {errors.text && touched.text && errors.text || errors.email && touched.email && errors.email || errors.name && touched.name && errors.name}</div>
                                 <button type="submit" disabled={false} className={style.sendButton}>Send
                                 </button>
                             </form>
